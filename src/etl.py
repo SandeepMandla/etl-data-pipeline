b493ca05b7c8b872bf036220617050fcd6e81897
Pooja Gazzela <pooja.gazzela06@gmail.com>
2026-06-28 03:10:59 +0530

    added MERGE logic for idempotent ETL pipeline

--- src/etl.py
+++ src/etl.py
@@ -1,32 +1,49 @@
-import pandas as pd
 import pyodbc
 
-print("Starting ETL")
+print("Starting Stage → Final Load")
 
-# 1. Extract
-df = pd.read_csv("data/employees.csv")
-
-# 2. Transform
-df = df.drop_duplicates()
-df["Name"] = df["Name"].str.upper()
-
-# 3. Connect to SQL Server (THIS is the missing link)
 conn = pyodbc.connect(
     "DRIVER={SQL Server};"
-    "SERVER=localhost;"
+    "SERVER=localhost\SQLEXPRESS;"
     "DATABASE=EmployeeDB;"
     "Trusted_Connection=yes;"
 )
 
 cursor = conn.cursor()
 
-# 4. Load into SQL (THIS is the real connection)
-for _, row in df.iterrows():
+# 1. READ from staging table
+cursor.execute("SELECT * FROM Employee_Stage")
+rows = cursor.fetchall()
+
+print(f"Rows fetched from stage: {len(rows)}")
+
+# 2. CLEAN + LOAD into FINAL table
+for row in rows:
+    employee_id = row.EmployeeID
+    name = row.Name.strip().upper()
+    department = row.Department.strip().upper()
+    salary = row.Salary
+
+    if salary is None or salary <= 0:
+        continue
+
     cursor.execute("""
-        INSERT INTO Employee_Stage (EmployeeID, Name, Department, Salary)
-        VALUES (?, ?, ?, ?)
-    """, row.EmployeeID, row.Name, row.Department, row.Salary)
+        MERGE Employees AS target
+        USING (SELECT ? AS EmployeeID, ? AS Name, ? AS Department, ? AS Salary) AS source
+        ON target.EmployeeID = source.EmployeeID
+
+        WHEN MATCHED THEN
+            UPDATE SET
+                Name = source.Name,
+                Department = source.Department,
+                Salary = source.Salary
+
+        WHEN NOT MATCHED THEN
+            INSERT (EmployeeID, Name, Department, Salary)
+            VALUES (source.EmployeeID, source.Name, source.Department, source.Salary);
+    """,
+    employee_id, name, department, salary)
 
 conn.commit()
 
-print("Data successfully loaded into SQL Server")
+print("Stage → Final load completed successfully")
